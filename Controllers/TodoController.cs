44d51b01786a550788101fd16e0fc6308a0bafd5
Gusti Sadhu <gstsadhu@gmail.com>
2021-10-13 08:06:16 +0700

    initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        [Route("TestRun")]
+        [HttpGet]
+
+        public ActionResult TestRun()
+        {
+            return Ok("success");
+        }
+
+    }
+}
